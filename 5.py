a841ad38da9eb12df890d603511820486d181556
Andres <andresinfo1@gmail.com>
2022-06-07 18:19:50 -0300

    rama prueba

--- 5.py
+++ 5.py
@@ -5,4 +5,7 @@ for i in range(1,numero+1):
     if numero % i == 0:
         divisores.append(i)
 
-print(divisores)
+if len(divisores) > 2:
+    print(str(divisores)[1:-1])
+else:
+    print('Es un numero primo')
